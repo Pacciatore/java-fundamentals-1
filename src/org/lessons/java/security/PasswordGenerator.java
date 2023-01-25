bf5a35add5b245fc8c0c633d08a872334d367ee5
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-25 14:53:49 +0100

    User info retrieval

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,49 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
+
+	public static void main(String[] args) {
+
+		String name = "";
+		String surname = "";
+		String color = "";
+		byte birthDay = 0;
+		byte birthMonth = 0;
+		short birthYear = 0;
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Benvenuto nel generatore di password!");
+
+		System.out.println("Inserire nome: ");
+		name = s.nextLine();
+
+		System.out.println("Inserire cognome: ");
+		surname = s.nextLine();
+
+		System.out.println("Inserire colore preferito: ");
+		color = s.nextLine();
+
+		System.out.println("Inserire giorno di nascita: ");
+		birthDay = s.nextByte();
+
+		System.out.println("Inserire mese di nascita: ");
+		birthMonth = s.nextByte();
+
+		System.out.println("Inserire anno di nascita: ");
+		birthYear = s.nextShort();
+
+		s.close();
+
+		System.out.println("----------------------------");
+		System.out.println("Riepilogo informazioni");
+
+		System.out.println("Nome: " + name);
+		System.out.println("Cognome: " + surname);
+		System.out.println("Colore preferito: " + color);
+		System.out.println("Data di nascita: " + birthDay + "/" + birthMonth + "/" + birthYear);
+
+	}
 
 }
