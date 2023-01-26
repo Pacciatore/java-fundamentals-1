268d3a0fdcedcb5223e207515016567608052492
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-26 14:21:47 +0100

    Definizione variabili e costanti, da implementare

--- src/org/lessons/java/train/CalcolaBiglietto.java
+++ src/org/lessons/java/train/CalcolaBiglietto.java
@@ -3,7 +3,18 @@ package org.lessons.java.train;
 public class CalcolaBiglietto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		float ticketCost = 0.21f; // Constant
+		float ticketTotal = 0f; // Variable
+
+		float juniorDiscount = 0.2f; // Constant
+		float seniorDiscount = 0.4f; // Constant
+
+		int juniorAgeUpperLimit = 18; // Constant
+		int seniorAge = 65; // Constant
+
+		float kmToTravel = 0f; // Variable
+		int passengerAge = 0; // Variable
 
 	}
 
