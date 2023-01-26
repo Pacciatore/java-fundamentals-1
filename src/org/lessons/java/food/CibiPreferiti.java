29d236f68902572e323b710cdce71ce78cd7db3b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-26 15:00:34 +0100

    Definizione cibi preferiti e show primo e ultimo posto

--- src/org/lessons/java/food/CibiPreferiti.java
+++ src/org/lessons/java/food/CibiPreferiti.java
@@ -4,6 +4,15 @@ public class CibiPreferiti {
 
 	public static void main(String[] args) {
 
+		String[] myFood = { "Pizza", "Lasagna", "Pasta e lenticchie", "Pasta al forno", "Bolognese" };
+
+		System.out.println("Elementi della classifica: " + myFood.length);
+		System.out.println("");
+
+		System.out.println("Primo posto in classifica: " + myFood[0]);
+
+		System.out.println("Ultimo posto in classifica: " + myFood[myFood.length - 1]);
+
 	}
 
 }
