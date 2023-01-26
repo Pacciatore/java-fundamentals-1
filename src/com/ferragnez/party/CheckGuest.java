9a472373894a0781181898a7760836f1c7e5ecef
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-26 15:16:14 +0100

    Creazione di whiteList array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -4,6 +4,9 @@ public class CheckGuest {
 
 	public static void main(String[] args) {
 
+		String[] whiteList = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+				"Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeili" };
+
 	}
 
 }
