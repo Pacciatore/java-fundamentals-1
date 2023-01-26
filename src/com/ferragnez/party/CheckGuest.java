b5210291c8a9471321f088facbd7f2397c77dc1f
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-26 15:23:07 +0100

    Uso del for per controllo whiteList

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -7,6 +7,7 @@ public class CheckGuest {
 	public static void main(String[] args) {
 
 		String name = "";
+		boolean found = false;
 
 		String[] whiteList = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
 				"Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeili" };
@@ -20,6 +21,16 @@ public class CheckGuest {
 		name = s.nextLine().trim();
 
 		s.close();
+
+		for (int i = 0; i < whiteList.length; i++) {
+			if (name.toLowerCase().equals(whiteList[i].toLowerCase()))
+				found = true;
+		}
+
+		if (found)
+			System.out.println("Prego, puoi entrare.");
+		else
+			System.out.println("Mi spiace, non sei in lista.");
 
 	}
 
