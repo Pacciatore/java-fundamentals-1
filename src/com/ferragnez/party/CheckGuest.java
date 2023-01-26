2d549bd15b00c8bca16af3613dc9eec85ed72105
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-26 15:20:10 +0100

    ottenimento del nome con trim()

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,11 +1,25 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
 	public static void main(String[] args) {
 
+		String name = "";
+
 		String[] whiteList = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
 				"Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeili" };
+
+		System.out.println("Benvenuto al Ferragnez Party!");
+		System.out.println("-+-+-+-+-+-+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+-+-+-+-+-+-");
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Come ti chiami?");
+		name = s.nextLine().trim();
+
+		s.close();
 
 	}
 
